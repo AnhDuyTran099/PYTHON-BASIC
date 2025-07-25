e1cfd1b2c776bc02e6bfbf934a6b766e8dc34196
babyforboss <duyanh25012010@gmail.com>
2025-07-25 11:32:31 +0700

    Update and rename bài23.py to bài23-Chu vi và diện tích hình tròn.py

---
+++ nhóm3/bài23-Chu vi và diện tích hình tròn.py
@@ -0,0 +1,4 @@
+import math
+r=float(input())
+pi=math.pi
+print(f"Chu vi: {2*pi*r:.2f}, Dien tich: {pi*(r**2):.2f}")
--- nhóm3/bài23.py
+++
@@ -1,4 +0,0 @@
-import math
-r=float(input())
-pi=math.pi
-print(f"Chu vi: {2*pi*r:.2f}, Dien tich: {pi*(r**2):.2f}")
