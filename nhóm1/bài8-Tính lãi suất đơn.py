88a4db63119cbbe001506703693f94903b581c2e
babyforboss <duyanh25012010@gmail.com>
2025-07-25 11:18:37 +0700

    Update and rename bài8.py to bài8-Tính lãi suất đơn.py

---
+++ nhóm1/bài8-Tính lãi suất đơn.py
@@ -0,0 +1,2 @@
+v,l,s=map(float,input().split())
+print(f"Lai: {int(v*l*s/100)}, Tong: {int(v+v*l*s/100)}")
--- nhóm1/bài8.py
+++
@@ -1,2 +0,0 @@
-v,l,s=map(float,input().split())
-print(f"Lai: {int(v*l*s/100)}, Tong: {int(v+v*l*s/100)}")
